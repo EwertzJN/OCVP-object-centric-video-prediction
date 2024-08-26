669367146d0ca47a6959aa3430d35ab6a62a8349
Jan Ewertz <s6jjewer@uni-bonn.de>
2024-08-26 18:26:07 +0200

    Adjusted parameter init. for learned init.

--- object_centric_video_prediction/models/initializers.py
+++ object_centric_video_prediction/models/initializers.py
@@ -115,7 +115,7 @@ class Learned(nn.Module):
         self.initial_slots = torch.nn.Parameter(torch.randn(1, self.num_slots, self.slot_dim))
 
         with torch.no_grad():
-            std = sqrt(6.0 / (1 + slot_dim))
+            std = sqrt(2.0 / (1 + slot_dim))
             torch.nn.init.normal_(self.initial_slots, 0, std)
         return
 
